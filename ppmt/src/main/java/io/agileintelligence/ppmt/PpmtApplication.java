9b3df2d74dc7867ac4118637177ed3c2d0dd80e7
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-15 13:51:14 -0400

    first commit

---
+++ ppmt/src/main/java/io/agileintelligence/ppmt/PpmtApplication.java
@@ -0,0 +1,12 @@
+package io.agileintelligence.ppmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtApplication.class, args);
+    }
+}
