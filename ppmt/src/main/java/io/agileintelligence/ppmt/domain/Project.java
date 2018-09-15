9a568a60ddb04b9570788f4de324ce8721b00ca8
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-15 15:02:59 -0400

    Added Validation to Project POJO

--- ppmt/src/main/java/io/agileintelligence/ppmt/domain/Project.java
+++ ppmt/src/main/java/io/agileintelligence/ppmt/domain/Project.java
@@ -1,6 +1,8 @@
 package io.agileintelligence.ppmt.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -10,8 +12,13 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min =4, max=5, message = "Please use 4 to 5 characters")
+    @Column(unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
     private String description;
     private Date start_date;
     private Date end_date;
