be85ecd372edbd801503a6f8a22cbc7373f2552a
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-16 11:59:15 -0400

    branch 4 work

--- ppmt/src/main/java/io/agileintelligence/ppmt/web/ProjectController.java
+++ ppmt/src/main/java/io/agileintelligence/ppmt/web/ProjectController.java
@@ -2,14 +2,23 @@ package io.agileintelligence.ppmt.web;
 
 
 import io.agileintelligence.ppmt.domain.Project;
+import io.agileintelligence.ppmt.service.MapValidationErrorsService;
 import io.agileintelligence.ppmt.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,8 +27,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorsService mapValidationErrorsService;
+
     @PostMapping("")
-    public ResponseEntity<?> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project , BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorsService.MapValidationErrorsService(result);
+        if(errorMap!=null)return errorMap;
+
         Project newProject = projectService.saveProject(project);
 
         return new ResponseEntity<Project>(newProject, HttpStatus.CREATED);
