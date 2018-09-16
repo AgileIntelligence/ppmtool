5a8f8acb4eeecd5d50b7f20d3853a68b8d225dcd
AgileIntelligence <io.agileintelligence@gmail.com>
2018-09-16 16:19:03 -0400

    Added delete operation for Project POJO

--- ppmt/src/main/java/io/agileintelligence/ppmt/service/ProjectService.java
+++ ppmt/src/main/java/io/agileintelligence/ppmt/service/ProjectService.java
@@ -43,5 +43,17 @@ public class ProjectService {
     }
 
 
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectNotFoundException("Cannot delete project: "+projectId+" as it doesn't exist");
+        }
+
+        projectRepository.delete(project);
+
+    }
+
+
 
 }
--- ppmt/src/main/java/io/agileintelligence/ppmt/web/ProjectController.java
+++ ppmt/src/main/java/io/agileintelligence/ppmt/web/ProjectController.java
@@ -47,4 +47,11 @@ public class ProjectController {
 
     @GetMapping("/all")
     public Iterable<Project> getAllProjects(){return projectService.findAllProjects();}
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project "+projectId+" deleted successfully", HttpStatus.OK);
+    }
 }
